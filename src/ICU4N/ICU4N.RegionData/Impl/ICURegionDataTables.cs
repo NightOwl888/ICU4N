dc0f39117a27f246cd7fa3c77afe8d597489184b
Shad Storhaug <shad@shadstorhaug.com>
2017-11-26 04:50:24 +0700

    Added RegionData project, which fixed many tests

---
+++ src/ICU4N/ICU4N.RegionData/Impl/ICURegionDataTables.cs
@@ -0,0 +1,10 @@
+﻿namespace ICU4N.Impl
+{
+    public class ICURegionDataTables : LocaleDisplayNamesImpl.ICUDataTables
+    {
+        public ICURegionDataTables()
+            : base(ICUData.ICU_REGION_BASE_NAME)
+        {
+        }
+    }
+}
