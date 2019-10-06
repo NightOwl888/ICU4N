296c6facc96b5972d567590c97e2fb21cee471cf
Shad Storhaug <shad@shadstorhaug.com>
2019-10-06 12:54:10 +0700

    ICU4N.Collation: Set InternalsVisibleTo for ICU4N.Tests.Collation

--- src/ICU4N.Collation/Properties/AssemblyInfo.cs
+++ src/ICU4N.Collation/Properties/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using System;
+﻿using ICU4N;
+using System;
+using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
+
+[assembly: InternalsVisibleTo("ICU4N.Tests.Collation, PublicKey=" + AssemblyKeys.PublicKey)]
