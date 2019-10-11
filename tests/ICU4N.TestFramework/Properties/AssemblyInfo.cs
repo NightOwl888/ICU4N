8a65a9c2c32f8612e7b6cd5ebadccea3075bcc8c
Shad Storhaug <shad@shadstorhaug.com>
2019-10-11 14:26:31 +0700

    Suppressed code analysis messages CA1034, CA1031, and CA1822 in tests

--- tests/ICU4N.TestFramework/Properties/AssemblyInfo.cs
+++ tests/ICU4N.TestFramework/Properties/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 ﻿using ICU4N;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ICU4N.Tests, PublicKey=" + AssemblyKeys.PublicKey)]
 [assembly: InternalsVisibleTo("ICU4N.Tests.Transliterator, PublicKey = " + AssemblyKeys.PublicKey)]
+
+[assembly: SuppressMessage("Microsoft.Design", "CA1034", Justification = "We don't care about Java-style classes in tests")]
