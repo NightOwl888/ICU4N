8e50c5813079f37020bb1e6384158aad3403fe22
Shad Storhaug <shad@shadstorhaug.com>
2020-02-09 02:47:29 +0700

    ICU4N.Impl.SoftCache: renamed Cache and factored out SoftReference<T>

--- src/ICU4N/Impl/SoftCache.cs
+++ src/ICU4N/Impl/SoftCache.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace ICU4N.Impl
 {
@@ -9,21 +9,21 @@ namespace ICU4N.Impl
     /// <remarks>
     /// To use, pass a delegate that implements the <see cref="CacheBase{TKey, TValue, TData}.CreateInstance(TKey, TData)"/> method.
     /// </remarks>
-    /// <typeparam name="K">Cache lookup key type</typeparam>
-    /// <typeparam name="V">Cache instance value type (must not be a CacheValue)</typeparam>
-    /// <typeparam name="D">Data type for creating a new instance value</typeparam>
-    /// <seealso cref="SoftCache{K, V, D}"/>
+    /// <typeparam name="TKey">Cache lookup key type.</typeparam>
+    /// <typeparam name="TValue">Cache instance value type (must not be a <see cref="CacheValue{V}"/>).</typeparam>
+    /// <typeparam name="TData">Data type for creating a new instance value.</typeparam>
+    /// <seealso cref="Cache{K, V, D}"/>
     /// <seealso cref="CacheBase{TKey, TValue, TData}"/>
     /// <author>Shad Storhaug</author>
-    public class AnonymousSoftCache<K, V, D> : SoftCache<K, V, D> where V : class
+    public class AnonymousCache<TKey, TValue, TData> : Cache<TKey, TValue, TData> where TValue : class
     {
-        private readonly Func<K, D, V> createInstance;
-        public AnonymousSoftCache(Func<K,D,V> createInstance)
+        private readonly Func<TKey, TData, TValue> createInstance;
+        public AnonymousCache(Func<TKey, TData, TValue> createInstance)
         {
             this.createInstance = createInstance ?? throw new ArgumentNullException(nameof(createInstance));
         }
 
-        protected override V CreateInstance(K key, D data)
+        protected override TValue CreateInstance(TKey key, TData data)
         {
             return createInstance(key, data);
         }
@@ -37,94 +37,38 @@ namespace ICU4N.Impl
     /// and call Get() with the key and the data. The Get() call will use the data
     /// only if it needs to call <see cref="CacheBase{TKey, TValue, TData}.CreateInstance(TKey, TData)"/>, otherwise the data is ignored.
     /// <para/>
-    /// When caching instances while the CacheValue "strength" is <see cref="CacheValueStrength.Soft"/>,
-    /// the runtime can later release these instances once they are not used any more at all.
-    /// If such an instance is then requested again, the <see cref="GetInstance(K, D)"/> method 
-    /// will call <see cref="CacheBase{TKey, TValue, TData}.CreateInstance(TKey, TData)"/> again and reset the CacheValue.
     /// The cache holds on to its map of keys to CacheValues forever.
     /// <para/>
-    /// A value can be null if <see cref="CacheBase{TKey, TValue, TData}.CreateInstance(TKey, TData)"/> returns null.
+    /// A value can be <c>null</c> if <see cref="CacheBase{TKey, TValue, TData}.CreateInstance(TKey, TData)"/> returns <c>null</c>.
     /// In this case, it must do so consistently for the same key and data.
     /// </remarks>
-    /// <typeparam name="K">Cache lookup key type</typeparam>
-    /// <typeparam name="V">Cache instance value type (must not be a CacheValue)</typeparam>
-    /// <typeparam name="D">Data type for creating a new instance value</typeparam>
+    /// <typeparam name="TKey">Cache lookup key type.</typeparam>
+    /// <typeparam name="TValue">Cache instance value type (must not be a <see cref="CacheValue{V}"/>).</typeparam>
+    /// <typeparam name="TData">Data type for creating a new instance value.</typeparam>
     /// <author>Markus Scherer, Mark Davis</author>
-    public abstract class SoftCache<K, V, D> : CacheBase<K, V, D> where V : class
+    // ICU4N: .NET has no "SoftRefernce", so we are always using strong references here.
+    // The below is greatly simplified by using a ConcurrentDictionary to do most of the heavy lifting.
+    public abstract class Cache<TKey, TValue, TData> : CacheBase<TKey, TValue, TData> where TValue : class
     {
-        //private ConcurrentDictionary<K, object> map = new ConcurrentDictionary<K, object>();
-        private IDictionary<K, object> map = new Dictionary<K, object>();
-        private object syncLock = new object();
+        private readonly ConcurrentDictionary<TKey, object> map = new ConcurrentDictionary<TKey, object>();
 
-        public override V GetInstance(K key, D data)
+        public override TValue GetInstance(TKey key, TData data)
         {
-            // We synchronize twice, once in the ConcurrentHashMap and
-            // once in valueRef.resetIfCleared(value),
-            // because we prefer the fine-granularity locking of the ConcurrentHashMap
-            // over coarser locking on the whole cache instance.
-            // We use a CacheValue (a second level of indirection) because
-            // ConcurrentHashMap.putIfAbsent() never replaces the key's value, and if it were
-            // a simple Reference we would not be able to reset its value after it has been cleared.
-            // (And ConcurrentHashMap.put() always replaces the value, which we don't want either.)
-            object mapValue = null;
-            lock (syncLock)
-                map.TryGetValue(key, out mapValue);
-            if (mapValue != null)
-            {
-                if (!(mapValue is CacheValue<V>))
-                {
-                    // The value was stored directly.
-                    return (V)mapValue;
-                }
-                CacheValue<V> cv = (CacheValue<V>)mapValue;
-                if (cv.IsNull)
-                {
-                    return null;
-                }
-                V value = cv.Get();
-                if (value != null)
-                {
-                    return value;
-                }
-                // The instance has been evicted, its Reference cleared.
-                // Create and set a new instance.
-                value = CreateInstance(key, data);
-                return cv.ResetIfCleared(value);
-            }
-            else /* valueRef == null */
+            object mapValue = map.GetOrAdd(key, (k) =>
             {
                 // We had never cached an instance for this key.
-                V value = CreateInstance(key, data);
-                mapValue = (value != null && CacheValue<V>.FutureInstancesWillBeStrong) ?
-                        value : (object)CacheValue<V>.GetInstance(value);
-                object temp;
-                lock (syncLock)
-                {
-                    // ICU4N TODO: use PutIfAbsent logic from elsewhere to utilize ConcurrentDictionary...?
-                    if (!map.TryGetValue(key, out temp))
-                    {
-                        // put if absent
-                        map[key] = mapValue;
-                    }
-                    mapValue = temp;
-                }
-                if (mapValue == null)
-                {
-                    // Normal "put": Our new value is now cached.
-                    return value;
-                }
-
-                // Race condition: Another thread beat us to putting a CacheValue
-                // into the map. Return its value, but just in case the garbage collector
-                // was aggressive, we also offer our new instance for caching.
-                if (!(mapValue is CacheValue<V>))
-                {
-                    // The value was stored directly.
-                    return (V)mapValue;
-                }
-                CacheValue<V> cv = (CacheValue<V>)mapValue;
-                return cv.ResetIfCleared(value);
+                return CreateInstance(key, data) ?? (object)CacheValue<TValue>.GetInstance(null);
+            });
+            if (!(mapValue is CacheValue<TValue> cv))
+            {
+                // The value was stored directly.
+                return (TValue)mapValue;
             }
+            if (cv.IsNull)
+            {
+                return null;
+            }
+            return cv.Get(); // Always a strong ref - no need to check for null and re-cache here
         }
     }
 }
--- tests/ICU4N.Tests/Dev/Test/Impl/CacheTest.cs
+++ tests/ICU4N.Tests/Dev/Test/Impl/CacheTest.cs
@@ -15,56 +15,59 @@ namespace ICU4N.Dev.Test.Impl
             CacheValue<object> nv = CacheValue<object>.GetInstance(null);
             assertTrue("null CacheValue isNull()", nv.IsNull);
             assertTrue("null CacheValue get()==null", nv.Get() == null);
-            assertTrue("null CacheValue reset==null", nv.ResetIfCleared(null) == null);
-            try
-            {
-                object v = nv.ResetIfCleared(this);
-                fail("null CacheValue reset(not null) should throw an Exception, returned " +
-                        v + " instead");
-            }
-            catch (Exception expected)
-            {
-            }
+            // ICU4N: ResetIfCleared/FutureInstancesWillBeStrong factored out, as we are always strong
+            //assertTrue("null CacheValue reset==null", nv.ResetIfCleared(null) == null);
+            //try
+            //{
+            //    object v = nv.ResetIfCleared(this);
+            //    fail("null CacheValue reset(not null) should throw an Exception, returned " +
+            //            v + " instead");
+            //}
+            //catch (Exception expected)
+            //{
+            //}
         }
 
         /** Code coverage for CacheValue. */
         [Test]
         public void TestStrongCacheValue()
         {
-            bool wasStrong = CacheValue<object>.FutureInstancesWillBeStrong;
-            CacheValue<object>.Strength = CacheValueStrength.Strong;
-            assertTrue("setStrength(STRONG).futureInstancesWillBeStrong()",
-                    CacheValue<object>.FutureInstancesWillBeStrong);
+            // ICU4N: ResetIfCleared/FutureInstancesWillBeStrong factored out, as we are always strong
+            //bool wasStrong = CacheValue<object>.FutureInstancesWillBeStrong;
+            //CacheValue<object>.Strength = CacheValueStrength.Strong;
+            //assertTrue("setStrength(STRONG).futureInstancesWillBeStrong()",
+            //        CacheValue<object>.FutureInstancesWillBeStrong);
             CacheValue<Object> sv = CacheValue<Object>.GetInstance(this);
             assertFalse("strong CacheValue not isNull()", sv.IsNull);
             assertTrue("strong CacheValue get()==same", sv.Get() == this);
-            // A strong CacheValue never changes value.
-            // The implementation does not check that the new value is equal to the old one,
-            // or even of equal type, so it does not matter which new value we pass in.
-            assertTrue("strong CacheValue reset==same", sv.ResetIfCleared("") == this);
-            if (!wasStrong)
-            {
-                CacheValue<object>.Strength = CacheValueStrength.Soft;
-            }
+            //// A strong CacheValue never changes value.
+            //// The implementation does not check that the new value is equal to the old one,
+            //// or even of equal type, so it does not matter which new value we pass in.
+            //assertTrue("strong CacheValue reset==same", sv.ResetIfCleared("") == this);
+            //if (!wasStrong)
+            //{
+            //    CacheValue<object>.Strength = CacheValueStrength.Soft;
+            //}
         }
 
-        /** Code coverage for CacheValue. */
-        [Test]
-        public void TestSoftCacheValue()
-        {
-            bool wasStrong = CacheValue<object>.FutureInstancesWillBeStrong;
-            CacheValue<object>.Strength = CacheValueStrength.Soft;
-            assertFalse("setStrength(SOFT).futureInstancesWillBeStrong()",
-                    CacheValue<object>.FutureInstancesWillBeStrong);
-            CacheValue<Object> sv = CacheValue<object>.GetInstance(this);
-            assertFalse("soft CacheValue not isNull()", sv.IsNull);
-            Object v = sv.Get();
-            assertTrue("soft CacheValue get()==same or null", v == this || v == null);
-            assertTrue("soft CacheValue reset==same", sv.ResetIfCleared(this) == this);
-            if (wasStrong)
-            {
-                CacheValue<object>.Strength = CacheValueStrength.Strong;
-            }
-        }
+        // ICU4N: ResetIfCleared/FutureInstancesWillBeStrong factored out, as we are always strong
+        ///** Code coverage for CacheValue. */
+        //[Test]
+        //public void TestSoftCacheValue()
+        //{
+        //    bool wasStrong = CacheValue<object>.FutureInstancesWillBeStrong;
+        //    CacheValue<object>.Strength = CacheValueStrength.Soft;
+        //    assertFalse("setStrength(SOFT).futureInstancesWillBeStrong()",
+        //            CacheValue<object>.FutureInstancesWillBeStrong);
+        //    CacheValue<Object> sv = CacheValue<object>.GetInstance(this);
+        //    assertFalse("soft CacheValue not isNull()", sv.IsNull);
+        //    Object v = sv.Get();
+        //    assertTrue("soft CacheValue get()==same or null", v == this || v == null);
+        //    assertTrue("soft CacheValue reset==same", sv.ResetIfCleared(this) == this);
+        //    if (wasStrong)
+        //    {
+        //        CacheValue<object>.Strength = CacheValueStrength.Strong;
+        //    }
+        //}
     }
 }
