9ca8b734ede3eda5b545760dd7c7919065b78259
Shad Storhaug <shad@shadstorhaug.com>
2020-03-30 23:18:15 +0700

    Modified BreakIterator to expose both CharacterIterator and ICharacterEnumerator overloads of SetText() to work through the issues on Lucene.NET one by one

--- tests/ICU4N.Tests.Collation/Dev/Test/Search/SearchTest.cs
+++ tests/ICU4N.Tests.Collation/Dev/Test/Search/SearchTest.cs
@@ -1,4 +1,5 @@
 ﻿using ICU4N.Globalization;
+using ICU4N.Support.Text;
 using ICU4N.Text;
 using ICU4N.Util;
 using J2N.Text;
--- tests/ICU4N.Tests/Dev/Test/Normalizers/ConformanceTest.cs
+++ tests/ICU4N.Tests/Dev/Test/Normalizers/ConformanceTest.cs
@@ -1,6 +1,7 @@
 ﻿using ICU4N.Globalization;
 using ICU4N.Impl;
 using ICU4N.Support;
+using ICU4N.Support.Text;
 using ICU4N.Text;
 using J2N.Text;
 using NUnit.Framework;
