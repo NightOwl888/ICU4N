cec1ce96376c2ad9321409034b7cb564015ab122
Shad Storhaug <shad@shadstorhaug.com>
2020-06-17 18:15:20 +0700

    region/language fixup

---
+++ src/ICU4N.LanguageData/Globalization/ICULanguageDataTableProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace ICU4N.Globalization
+{
+    /// <summary>
+    /// The data table provider for language data that is read from embedded resources
+    /// inside of this assembly.
+    /// <para/>
+    /// The data must reside in an embedded resource in a folder named <c>Impl\Data\lang\</c>
+    /// within the assembly, so it is resolved as <c>[assembly name].Impl.Data.lang.[localeID].res</c>.
+    /// </summary>
+    public class ICULanguageDataTableProvider : LanguageDataTableProvider
+    {
+#if FEATURE_TYPEEXTENSIONS_GETTYPEINFO
+        public ICULanguageDataTableProvider()
+
+            : base(typeof(ICULanguageDataTableProvider).GetTypeInfo().Assembly)
+#else
+        private ICULanguageDataTableProvider()
+            : base(typeof(ICULanguageDataTableProvider).Assembly)
+#endif
+        { }
+
+        /// <summary>
+        /// The singleton instance of the language data table provider.
+        /// </summary>
+        public static ICULanguageDataTableProvider Instance => new ICULanguageDataTableProvider();
+    }
+}
