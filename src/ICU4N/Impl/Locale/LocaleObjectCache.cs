a417830e998d4dad2238b0e1b693f25d5dd9c141
Shad Storhaug <shad@shadstorhaug.com>
2021-03-06 19:55:28 +0700

    ICU4N.Impl.Locale.LocaleObjectCache: Made parameter of LurchTable into a Lazy<T> to ensure two threads cannot load the cache at the same time. (fixes #31)

--- src/ICU4N/Impl/Locale/LocaleObjectCache.cs
+++ src/ICU4N/Impl/Locale/LocaleObjectCache.cs
@@ -1,10 +1,11 @@
 ﻿using J2N.Collections.Concurrent;
+using System;
 
 namespace ICU4N.Impl.Locale
 {
     public abstract class LocaleObjectCache<TKey, TValue> where TValue : class
     {
-        private readonly LurchTable<TKey, TValue> _map;
+        private readonly LurchTable<TKey, Lazy<TValue>> _map;
 
         public LocaleObjectCache()
             : this(16)
@@ -16,12 +17,13 @@ namespace ICU4N.Impl.Locale
             // ICU4N: Since .NET doesn't have a memory-sensitive cache, we are using an LRU cache with a fixed size.
             // This ensures that the culture(s) that the application uses most stay near the top of the cache and
             // less used cultures get popped off of the bottom of the cache.
-            _map = new LurchTable<TKey, TValue>(initialCapacity, LurchTableOrder.Access, limit: 64, comparer: null);
+            _map = new LurchTable<TKey, Lazy<TValue>>(initialCapacity, LurchTableOrder.Access, limit: 64, comparer: null);
         }
 
         public virtual TValue Get(TKey key)
         {
-            return _map.GetOrAdd(key, CreateObject);
+            var result = _map.GetOrAdd(key, (key) => new Lazy<TValue>(() => CreateObject(key)));
+            return result.Value;
         }
 
         protected abstract TValue CreateObject(TKey key);
