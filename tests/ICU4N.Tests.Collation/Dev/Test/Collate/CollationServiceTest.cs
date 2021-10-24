528e3e5b7854c10f471d0a92548f1d737ea879e0
Shad Storhaug <shad@shadstorhaug.com>
2021-10-24 21:06:46 +0700

    Another test?

--- tests/ICU4N.Tests.Collation/Dev/Test/Collate/CollationServiceTest.cs
+++ tests/ICU4N.Tests.Collation/Dev/Test/Collate/CollationServiceTest.cs
@@ -18,16 +18,28 @@ namespace ICU4N.Dev.Test.Collate
         public void TestRegister()
         {
 #if NET5_0_OR_GREATER
-            
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                Console.WriteLine("This is Linux! (RuntimeInformation)");
-            else
-                Console.WriteLine("This is NOT Linux! (RuntimeInformation)");
+
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    throw new Exception("This is Linux! (RuntimeInformation)");
+                else
+                    throw new Exception("This is NOT Linux! (RuntimeInformation)");
+            }
+            catch (Exception e)
+            {
+                if (OperatingSystem.IsLinux())
+                    throw new Exception("This is Linux! (OperatingSystem)", e);
+                else
+                    throw new Exception("This is NOT Linux! (OperatingSystem)", e);
+            }
 
             if (OperatingSystem.IsLinux())
-                Console.WriteLine("This is Linux! (OperatingSystem)");
+                throw new Exception("This is Linux! (OperatingSystem) 2");
             else
-                Console.WriteLine("This is NOT Linux! (OperatingSystem)");
+                throw new Exception("This is NOT Linux! (OperatingSystem) 2");
+
+
 
             Assume.That(!RuntimeInformation.IsOSPlatform(OSPlatform.Linux), "ICU4N TODO: Fails on Ubuntu 18.04 and Ubuntu 20.04 on .NET 5 and higher. See: https://github.com/NightOwl888/ICU4N/issues/37");
 #endif
