26c5503b318e08f1e292f791a540437265273e9f
Shad Storhaug <shad@shadstorhaug.com>
2022-03-14 02:47:28 +0700

    Added ICU4J Resource Converter tool to transform resource files for packing into satellite assemblies

---
+++ src/tools/ICU4JResourceConverter/JarUtil.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JavaResourceConverter
+{
+    /// <summary>
+    /// Utilities for working with ICU4J resources.
+    /// </summary>
+    public static class JarUtil
+    {
+        /// <summary>
+        /// The location within the .jar file where the data can be found.
+        /// </summary>
+        public const string DataPath = @"com\ibm\icu\impl\data";
+        public const string DataPrefix = "icudt";
+        public const string DataSuffix = "b";
+
+
+        public static void UnzipJar(string sourceFilePath, string destinationPath)
+        {
+            using var source = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            Unzip(source, new DirectoryInfo(destinationPath));
+        }
+
+        public static string GetDataDirectory(string icu4jHomePath, string majorVersion)
+        {
+            return Path.Combine(icu4jHomePath, DataPath, string.Concat(DataPrefix, majorVersion, DataSuffix));
+        }
+
+        //public static string GetICU4JMajorMinorVersion(string sourceFilePath)
+        //{
+        //    var attributes = File.GetAttributes(sourceFilePath);
+        //    if ((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+        //        throw new ArgumentException($"The {nameof(sourceFilePath)} must be a Java .jar file.");
+        //    if (Path.GetExtension(sourceFilePath) != ".jar")
+        //        throw new ArgumentException($"The {nameof(sourceFilePath)} must be a Java .jar file.");
+        //    var fileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+        //    if (!fileName.StartsWith("icu4j-", StringComparison.InvariantCultureIgnoreCase))
+        //        throw new ArgumentException($"The {nameof(sourceFilePath)} must start with 'icu4j-'.");
+
+        //    var match = Regex.Match(fileName, @"-(?<version>\d+\.\d+)$");
+        //    if (match.Success)
+        //        return match.Groups["version"].Value;
+        //    throw new ArgumentException("The file name must end in a major.minor version number.");
+        //}
+
+        public static string GetICU4JMajorVersion(string sourceFilePath)
+        {
+            var attributes = File.GetAttributes(sourceFilePath);
+            if ((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                throw new ArgumentException($"The {nameof(sourceFilePath)} must be a Java .jar file.");
+            if (Path.GetExtension(sourceFilePath) != ".jar")
+                throw new ArgumentException($"The {nameof(sourceFilePath)} must be a Java .jar file.");
+            var fileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+            if (!fileName.StartsWith("icu4j-", StringComparison.InvariantCultureIgnoreCase))
+                throw new ArgumentException($"The {nameof(sourceFilePath)} must start with 'icu4j-'.");
+
+            var match = Regex.Match(fileName, @"-(?<version>\d+)\.\d+$");
+            if (match.Success)
+                return match.Groups["version"].Value;
+            throw new ArgumentException("The file name must end in a major.minor version number.");
+        }
+
+        /// <summary>
+        /// Deletes one or more files or directories (and everything underneath it).
+        /// </summary>
+        /// <exception cref="IOException">If any of the given files (or their subhierarchy files in case
+        /// of directories) cannot be removed.</exception>
+        public static void Rm(params FileSystemInfo[] locations)
+        {
+            ISet<FileSystemInfo> unremoved = Rm(new HashSet<FileSystemInfo>(), locations);
+            if (unremoved.Count > 0)
+            {
+                StringBuilder b = new StringBuilder("Could not remove the following files (in the order of attempts):\n");
+                foreach (var f in unremoved)
+                {
+                    b.Append("   ")
+                     .Append(f.FullName)
+                     .Append("\n");
+                }
+                throw new IOException(b.ToString());
+            }
+        }
+
+        private static ISet<FileSystemInfo> Rm(ISet<FileSystemInfo> unremoved, params FileSystemInfo[] locations)
+        {
+            foreach (FileSystemInfo location in locations)
+            {
+                // LUCENENET: Refresh the state of the FileSystemInfo object so we can be sure
+                // the Exists property is (somewhat) current.
+                location.Refresh();
+
+                if (location.Exists)
+                {
+                    if (location is DirectoryInfo directory)
+                    {
+                        // Try to delete all of the files and folders in the directory
+                        Rm(unremoved, directory.GetFileSystemInfos());
+                    }
+
+                    try
+                    {
+                        location.Delete();
+                    }
+                    catch (Exception)
+                    {
+                        unremoved.Add(location);
+                    }
+                }
+            }
+            return unremoved;
+        }
+
+        /// <summary>
+        /// Convenience method unzipping <paramref name="zipFileStream"/> into <paramref name="destDir"/>, cleaning up
+        /// <paramref name="destDir"/> first. 
+        /// </summary>
+        public static void Unzip(Stream zipFileStream, DirectoryInfo destDir)
+        {
+            Rm(destDir);
+            destDir.Create();
+
+            using ZipArchive zip = new ZipArchive(zipFileStream);
+            foreach (var entry in zip.Entries)
+            {
+                // Ignore internal folders - these are tacked onto the FullName anyway
+                if (entry.FullName.EndsWith("/", StringComparison.Ordinal) || entry.FullName.EndsWith("\\", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+                using Stream input = entry.Open();
+                FileInfo targetFile = new FileInfo(NormalizePath(Path.Combine(destDir.FullName, entry.FullName)));
+                if (!targetFile.Directory.Exists)
+                {
+                    targetFile.Directory.Create();
+                }
+
+                using Stream output = new FileStream(targetFile.FullName, FileMode.Create, FileAccess.Write);
+                input.CopyTo(output);
+            }
+        }
+
+        /// <summary>
+        /// Normalize file path names
+        /// for the current operating system.
+        /// </summary>
+        public static string NormalizePath(string input)
+        {
+            if (Path.DirectorySeparatorChar.Equals('/'))
+            {
+                return input.Replace('\\', '/');
+            }
+            return input.Replace('/', '\\');
+        }
+    }
+}
---
+++ src/tools/ICU4JResourceConverter/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace JavaResourceConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appName = Assembly.GetEntryAssembly().GetName().Name;
+            var notAlreadyRunning = true;
+            using (var mutex = new Mutex(true, appName + "Singleton", out notAlreadyRunning))
+            {
+                if (notAlreadyRunning)
+                {
+                    //// do work here:
+                    //Console.WriteLine("Running. Press any key to exit...");
+                    //Console.ReadKey();
+
+                    string source;
+                    string workingDir = null;
+                    string outputDir = null;
+
+                    if (args is null || args.Length == 0)
+                        Usage();
+
+                    var arg0 = args[0];
+                    if (arg0.Equals("-help", StringComparison.Ordinal) || arg0.Equals("-?", StringComparison.Ordinal) || arg0.Equals("?", StringComparison.Ordinal))
+                        Usage();
+
+                    source = arg0; // Source .jar file
+
+                    for (int i = 1; i < args.Length; i++)
+                    {
+                        var arg = args[i];
+                        if (arg.Equals("-work", StringComparison.Ordinal))
+                            workingDir = i + 1 < args.Length ? args[i + 1] : throw new ArgumentException("Missing value for working directory.");
+                        else if (arg.Equals("-out", StringComparison.Ordinal))
+                            outputDir = i + 1 < args.Length ? args[i + 1] : throw new ArgumentException("Missing value for output directory.");
+                    }
+
+
+
+
+                    if (string.IsNullOrWhiteSpace(workingDir))
+                        workingDir = Path.GetTempPath() + Guid.NewGuid().ToString();
+
+                    workingDir = JarUtil.NormalizePath(workingDir);
+
+                    if (string.IsNullOrWhiteSpace(outputDir))
+                        outputDir = Path.Combine(workingDir, "resources");
+
+                    outputDir = JarUtil.NormalizePath(outputDir);
+
+                    //string source = @"F:\icu4j-60.1.jar";
+                    //string workingDir = @"F:\icu4j-temp";
+                    string icuDir = Path.Combine(workingDir, "icu-temp");
+
+                    icuDir = JarUtil.NormalizePath(icuDir);
+
+                    // ****** UNJAR COMMAND ******
+
+                    // TODO: Allow a switch to force unpack (or make it the default and switch for off)
+                    if (!Directory.Exists(icuDir))
+                    {
+                        // Unpack the .jar file
+                        JarUtil.UnzipJar(source, icuDir);
+                        Console.WriteLine($"Unzipped {source} to {icuDir}");
+                    }
+                    //string dataDir = UnJar(source, icuDir);
+
+                    // ****** TRANSFORM COMMAND ******
+
+                    string majorVersion = JarUtil.GetICU4JMajorVersion(source);
+                    string dataDir = JarUtil.GetDataDirectory(icuDir, majorVersion);
+
+                    Console.WriteLine($"ICU Data is available at {dataDir}");
+
+                    ResourceUtil.TransformResources(dataDir, outputDir);
+
+                    Console.WriteLine($"Transforming resources completed. Data is available in {outputDir}.");
+
+                    // ****** PACK COMMAND ****** (does assembly linking)
+
+
+                }
+                else
+                    Console.Error.WriteLine(appName + " is already running.");
+            }
+        }
+
+        private static void Usage()
+        {
+            Console.WriteLine("\n\n" + "ICU4JResourceConverter <icu4j .jar file> [-out <output directory>] [-work <working directory>]");
+            Environment.Exit(1);
+        }
+    }
+}
