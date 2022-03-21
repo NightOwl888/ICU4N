a1a475151908c88a4e0a0bd415e650bd0695cb22
Shad Storhaug <shad@shadstorhaug.com>
2022-03-21 08:07:43 +0700

    Merged ICU4N.LanguageData and ICU4N.RegionData into ICU4N

--- src/ICU4N/Support/Globalization/LocaleDisplayNames/DataTables/DefaultLanguageDataTableProvider.cs
+++ src/ICU4N/Support/Globalization/LocaleDisplayNames/DataTables/DefaultLanguageDataTableProvider.cs
@@ -1,23 +1,17 @@
 ﻿// Port of text.LocaleDisplayNamesImpl.DataTables from ICU4J
 // In .NET we use an interface to determine which data we are loading (so they cannot be plugged in to the wrong place).
 
-using ICU4N.Impl;
-using System;
-using System.Globalization;
 using System.Reflection;
 
 namespace ICU4N.Globalization
 {
     internal class DefaultLanguageDataTableProvider : ILanguageDataTableProvider
     {
-        private const string InstancePropertyMethodName = "get_Instance";
-        private readonly ILanguageDataTableProvider defaultDataTableProvider = Load(DataTableCultureDisplayNames.DefaultLanguageDataTableProvider);
+        private readonly ILanguageDataTableProvider defaultDataTableProvider = ICULanguageDataTableProvider.Instance;
 
-        public Assembly Assembly => defaultDataTableProvider != null
-            ? defaultDataTableProvider.Assembly
-            : ICUResourceBundle.IcuDataAssembly;
+        public Assembly Assembly => defaultDataTableProvider.Assembly;
 
-        public bool HasData => defaultDataTableProvider != null && defaultDataTableProvider.HasData;
+        public bool HasData => defaultDataTableProvider.HasData;
 
         public IDataTable GetDataTable(UCultureInfo culture, bool nullIfNotFound)
         {
@@ -25,26 +19,6 @@ namespace ICU4N.Globalization
                 return defaultDataTableProvider.GetDataTable(culture, nullIfNotFound);
 
             return new DataTable(nullIfNotFound);
-        }
-
-        private static ILanguageDataTableProvider Load(string className)
-        {
-            try
-            {
-                Type type = Type.GetType(className);
-                if (type == null)
-                    return null;
-
-#if FEATURE_TYPEEXTENSIONS_GETTYPEINFO
-                return (ILanguageDataTableProvider)Activator.CreateInstance(type);
-#else
-                return (ILanguageDataTableProvider)type.GetMethod(InstancePropertyMethodName).Invoke(null, null);
-#endif
-            }
-            catch (Exception)
-            {
-                return null;
-            }
         }
     }
 }
