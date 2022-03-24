e9e69d014447a30b82510e0e13aed2c2ceca9e85
Shad Storhaug <shad@shadstorhaug.com>
2022-03-25 03:55:27 +0700

    Revert "Updated ICUResourceBundle to check for data files outside of satellite assemblies"
    This reverts commit 82e12986cae8f43cac20e816de9619c908efb981.

--- src/ICU4N/ICUConfig.Designer.cs
+++ src/ICU4N/ICUConfig.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace ICU4N {
     using System;
+    using System.Reflection;
     
     
     /// <summary>
@@ -39,7 +40,11 @@ namespace ICU4N {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
+#if FEATURE_TYPEEXTENSIONS_GETTYPEINFO
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ICU4N.ICUConfig", typeof(ICUConfig).GetTypeInfo().Assembly);
+#else
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ICU4N.ICUConfig", typeof(ICUConfig).Assembly);
+#endif
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -70,7 +75,7 @@ namespace ICU4N {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to data/icudt60b.
+        ///   Looks up a localized string similar to .
         /// </summary>
         internal static string ICUBinary_DataPath {
             get {
