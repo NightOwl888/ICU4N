5603bd88865be871a9e8cfc26162d7a4f1481074
Shad Storhaug <shad@shadstorhaug.com>
2022-04-25 04:26:36 +0700

    CollationDataBuilder::GetCEs(): Allocate the correct amount of space in the StringBuilder

--- src/ICU4N.Collation/Impl/Coll/CollationDataBuilder.cs
+++ src/ICU4N.Collation/Impl/Coll/CollationDataBuilder.cs
@@ -388,7 +388,7 @@ namespace ICU4N.Impl.Coll
             }
             else
             {
-                return GetCEs(new StringBuilder(prefix.Length).Append(prefix).Append(s).AsCharSequence(), prefixLength, ces, cesLength);
+                return GetCEs(new StringBuilder(prefix.Length + s.Length).Append(prefix).Append(s).AsCharSequence(), prefixLength, ces, cesLength);
             }
         }
 
