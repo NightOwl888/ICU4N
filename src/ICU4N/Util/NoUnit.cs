5f8b48d6de95b9e38d9d5474964a865c48835726
Shad Storhaug <shad@shadstorhaug.com>
2022-12-23 11:00:59 +0700

    Ported ICU4N.Util.MeasureUnit, TimeUnit, NoUnit

---
+++ src/ICU4N/Util/NoUnit.cs
@@ -0,0 +1,50 @@
+﻿using ICU4N.Numerics;
+
+namespace ICU4N.Util
+{
+    /// <summary>
+    /// Dimensionless unit for percent and permille.
+    /// </summary>
+    /// <seealso cref="NumberFormatter"/>
+    /// <draft>ICU 60</draft>
+    /// <provisional>This API might change or be removed in a future release.</provisional>
+    internal class NoUnit : MeasureUnit // ICU4N TODO: API - this was public in ICU4J
+    {
+        //private static final long serialVersionUID = 2467174286237024095L;
+
+        /// <summary>
+        /// Constant for the base unit (dimensionless and no scaling).
+        /// </summary>
+        /// <draft>ICU 60</draft>
+        /// <provisional>This API might change or be removed in a future release.</provisional>
+        public static readonly NoUnit BASE = (NoUnit)MeasureUnit.InternalGetInstance("none", "base");
+
+        /// <summary>
+        /// Constant for the percent unit, or 1/100 of a base unit.
+        /// </summary>
+        /// <draft>ICU 60</draft>
+        /// <provisional>This API might change or be removed in a future release.</provisional>
+        public static readonly NoUnit PERCENT = (NoUnit)MeasureUnit.InternalGetInstance("none", "percent");
+
+        /// <summary>
+        /// Constant for the permille unit, or 1/100 of a base unit.
+        /// </summary>
+        /// <draft>ICU 60</draft>
+        /// <provisional>This API might change or be removed in a future release.</provisional>
+        public static readonly NoUnit PERMILLE = (NoUnit)MeasureUnit.InternalGetInstance("none", "permille");
+
+        /// <summary>
+        /// Package local constructor. This class is not designed for subclassing
+        /// by ICU users.
+        /// </summary>
+        /// <param name="subType">The unit subtype.</param>
+        /// <draft>ICU 60</draft>
+        /// <provisional>This API might change or be removed in a future release.</provisional>
+        internal NoUnit(string subType)
+#pragma warning disable CS0618 // Type or member is obsolete
+            : base("none", subType)
+#pragma warning restore CS0618 // Type or member is obsolete
+        {
+        }
+    }
+}
---
+++ src/ICU4N/Util/TimeUnit.cs
@@ -0,0 +1,67 @@
+﻿using J2N.Collections.Generic.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace ICU4N.Util
+{
+    /// <summary>
+    /// Measurement unit for time units.
+    /// </summary>
+    ///// <seealso cref="TimeUnitAmount"/>
+    /// <stable>ICU 4.0</stable>
+    internal class TimeUnit : MeasureUnit // ICU4N TODO: API - this was public in ICU4J
+    {
+        //private static final long serialVersionUID = -2839973855554750484L;
+
+        /**
+         * Here for serialization backward compatibility only.
+         */
+        private readonly int index;
+
+        internal TimeUnit(string type, string code)
+            : base(type, code)
+        {
+            index = 0;
+        }
+
+        private static readonly IList<TimeUnit> values = new TimeUnit[] { Second, Minute, Hour, Day, Week, Month, Year }.AsReadOnly();
+
+        /// <summary>
+        /// Gets the available values.
+        /// </summary>
+        /// <stable>ICU 4.0</stable>
+        public static IList<TimeUnit> Values => values;
+
+
+        // ICU4N TODO: Serialization
+        //private object WriteReplace() // throws ObjectStreamException
+        //{
+        //    return new MeasureUnitProxy(type, subType);
+        //}
+
+        //// For backward compatibility only
+        //private Object ReadResolve() //throws ObjectStreamException
+        //{
+        //    // The old index field used to uniquely identify the time unit.
+        //    switch (index)
+        //    {
+        //        case 6:
+        //            return Second;
+        //        case 5:
+        //            return Minute;
+        //        case 4:
+        //            return Hour;
+        //        case 3:
+        //            return Day;
+        //        case 2:
+        //            return Week;
+        //        case 1:
+        //            return Month;
+        //        case 0:
+        //            return Year;
+        //        default:
+        //            throw new Exception("Bad index: " + index); //new InvalidObjectException("Bad index: " + index);
+        //    }
+        //}
+    }
+}
