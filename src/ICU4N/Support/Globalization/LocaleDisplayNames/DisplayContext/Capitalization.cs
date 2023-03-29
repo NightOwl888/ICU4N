1d62cb5446971df377559e0f7849fda5c05cda04
Shad Storhaug <shad@shadstorhaug.com>
2023-03-29 23:28:48 +0700

    ICU4N.Globalization.Capitialization: Updated docs

--- src/ICU4N/Support/Globalization/LocaleDisplayNames/DisplayContext/Capitalization.cs
+++ src/ICU4N/Support/Globalization/LocaleDisplayNames/DisplayContext/Capitalization.cs
@@ -10,32 +10,32 @@ namespace ICU4N.Globalization
         /// <summary>
         /// The capitalization context to be used is unknown (this is the default value).
         /// </summary>
-        /// <draft>ICU 60</draft>
+        /// <draft>ICU 60.1</draft>
         None,
         /// <summary>
         /// The capitalization context if a date, date symbol or display name is to be
         /// formatted with capitalization appropriate for the middle of a sentence.
         /// </summary>
-        /// <draft>ICU 60</draft>
+        /// <draft>ICU 60.1</draft>
         MiddleOfSentence,
         /// <summary>
         /// The capitalization context if a date, date symbol or display name is to be
         /// formatted with capitalization appropriate for the beginning of a sentence.
         /// </summary>
-        /// <draft>ICU 60</draft>
+        /// <draft>ICU 60.1</draft>
         BeginningOfSentence,
         /// <summary>
         /// The capitalization context if a date, date symbol or display name is to be
         /// formatted with capitalization appropriate for a user-interface list or menu item.
         /// </summary>
-        /// <draft>ICU 60</draft>
+        /// <draft>ICU 60.1</draft>
         UIListOrMenu,
         /// <summary>
         /// The capitalization context if a date, date symbol or display name is to be
         /// formatted with capitalization appropriate for stand-alone usage such as an
         /// isolated name on a calendar page.
         /// </summary>
-        /// <draft>ICU 60</draft>
+        /// <draft>ICU 60.1</draft>
         Standalone,
     }
 
