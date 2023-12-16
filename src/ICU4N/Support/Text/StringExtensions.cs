1485c7d7a62c0141ae692d8827f876ccfbea4e6f
Shad Storhaug <shad@shadstorhaug.com>
2023-12-16 18:42:06 +0700

    ICU4N.Support.Text.StringExtensions: Added ContentEquals() overloads for ReadOnlySpan<char> that are missing from J2N.

--- src/ICU4N/Support/Text/StringExtensions.cs
+++ src/ICU4N/Support/Text/StringExtensions.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using J2N.Text;
+using System;
 using System.Runtime.CompilerServices;
+using System.Text;
+#nullable enable
 
 namespace ICU4N.Support.Text
 {
@@ -47,6 +50,66 @@ namespace ICU4N.Support.Text
                 retVal = s.AsSpan(0, s.Length).TryCopyTo(destination);
             }
             return retVal;
+        }
+
+
+        /// <summary>
+        /// Compares a <see cref="ReadOnlySpan{Char}"/> to this <see cref="string"/> to determine if
+        /// their contents are equal.
+        /// <para/>
+        /// This differs from <see cref="string.Equals(string, StringComparison)"/> in that it does not
+        /// consider the <see cref="string"/> type to be part of the comparison - it will match for any character sequence
+        /// that contains matching characters.
+        /// <para/>
+        /// The comparison is done using <see cref="StringComparison.Ordinal"/> comparison rules.
+        /// </summary>
+        /// <param name="text">This <see cref="string"/>.</param>
+        /// <param name="charSequence">The character sequence to compare to.</param>
+        /// <returns><c>true</c> if this <see cref="string"/> represents the same
+        /// sequence of characters as the specified <paramref name="charSequence"/>; otherwise, <c>false</c>.</returns>
+#if FEATURE_METHODIMPLOPTIONS_AGRESSIVEINLINING
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif 
+        public static bool ContentEquals(this string? text, ReadOnlySpan<char> charSequence) // ICU4N TODO: Move to J2N
+        {
+            if (text is null)
+                return false;
+
+            int len = charSequence.Length;
+            if (len != text.Length)
+                return false;
+            if (len == 0 && text.Length == 0)
+                return true; // since both are empty strings
+
+            return charSequence.EqualsOrdinal(text.AsSpan());
+        }
+
+        /// <summary>
+        /// Compares a <see cref="ReadOnlySpan{Char}"/> to this <see cref="string"/> to determine if
+        /// their contents are equal.
+        /// <para/>
+        /// This differs from <see cref="string.Equals(string, StringComparison)"/> in that it does not
+        /// consider the <see cref="string"/> type to be part of the comparison - it will match for any character sequence
+        /// that contains matching characters.
+        /// </summary>
+        /// <param name="text">This <see cref="string"/>.</param>
+        /// <param name="charSequence">The character sequence to compare to.</param>
+        /// <param name="comparisonType">One of the enumeration values that specifies the rules for the comparison.</param>
+        /// <returns><c>true</c> if this <see cref="string"/> represents the same
+        /// sequence of characters as the specified <paramref name="charSequence"/>; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentException"><paramref name="comparisonType"/> is not a <see cref="StringComparison"/> value.</exception>
+        public static bool ContentEquals(this string? text, ReadOnlySpan<char> charSequence, StringComparison comparisonType) // ICU4N TODO: Move to J2N
+        {
+            if (text is null)
+                return false;
+
+            int len = charSequence.Length;
+            if (len != text.Length)
+                return false;
+            if (len == 0 && text.Length == 0)
+                return true; // since both are empty strings
+
+            return charSequence.Equals(text.AsSpan(), comparisonType);
         }
 #endif
     }
