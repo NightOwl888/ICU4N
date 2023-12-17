fd9eb03dab46d4db6a6235318cf65220594e078c
Shad Storhaug <shad@shadstorhaug.com>
2023-12-17 08:45:13 +0700

    ICU4N.Support.Text.StringBuilderExtensions: Added Append() overload for ReadOnlySpan<char>

--- src/ICU4N/Support/Text/StringBuilderExtensions.cs
+++ src/ICU4N/Support/Text/StringBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using J2N.Text;
+using System;
 using System.Text;
 
 namespace ICU4N.Support.Text
@@ -19,5 +20,37 @@ namespace ICU4N.Support.Text
         {
             return new StringBuilderCharSequence(text);
         }
+
+#if FEATURE_SPAN
+        /// <summary>
+        /// Appends the given <see cref="ReadOnlySpan{Char}"/> to this <see cref="StringBuilder"/>.
+        /// </summary>
+        /// <param name="text">This <see cref="StringBuilder"/>.</param>
+        /// <param name="charSequence">The <see cref="ReadOnlySpan{Char}"/> to append.</param>
+        /// <returns>This <see cref="StringBuilder"/>, for chaining.</returns>
+        /// <exception cref="ArgumentNullException">If <paramref name="text"/> is <c>null</c>.</exception>
+        public static StringBuilder Append(this StringBuilder text, ReadOnlySpan<char> charSequence) // ICU4N TODO: Move to J2N
+        {
+            if (text is null)
+                throw new ArgumentNullException(nameof(text));
+
+#if FEATURE_STRINGBUILDER_APPEND_CHARPTR
+            unsafe
+            {
+                fixed (char* seq = charSequence)
+                {
+                    text.Append(seq, charSequence.Length);
+                }
+            }
+#else
+            text.EnsureCapacity(text.Length + charSequence.Length);
+            foreach (var ch in charSequence)
+            {
+                text.Append(ch);
+            }
+#endif
+            return text;
+        }
+#endif
     }
 }
